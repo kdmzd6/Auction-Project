a2b15e12a130d87bd72234e9b94519861f2aca17
unknown <Sean@Sean-PC.(none)>
2012-05-14 22:35:06 -0500

    js for +ing bidder & html for +ing table number

---
+++ addbidder.js
@@ -0,0 +1,14 @@
+  document.getElementById("Enter").onclick=function addbidder() {
+    var tn = document.getElementById('tablenumber').value
+    var sl = document.getElementById("seatletter").value
+    var fn = document.getElementById('firstname').value
+    var ln = document.getElementById('lastname').value
+    
+    var st = document.getElementById('seattable')
+    var r1c1 = document.getElementById('r1c1')
+  r1c1 = tn
+  alert
+
+
+  }
+
